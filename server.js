b5339abd3c8e9f1202a5d781d4d7333baf9f0af6
Paul Bellamy <paul.a.bellamy@gmail.com>
2011-02-25 11:15:46 +0000

    Adding run_dev_server and server.js and basic README

---
+++ server.js
@@ -0,0 +1,19 @@
+require.paths.unshift(__dirname + "/vendor");
+
+process.addListener('uncaughtException', function (err, stack) {
+  console.log('------------------------');
+  console.log('Exception: ' + err);
+  console.log(err.stack);
+  console.log('------------------------');
+});
+/*
+var NodeMap = require('./lib/nodemap');
+
+new NodeMap({
+  port: 8000,
+  geoipServer: {
+      hostname: 'geoip.pachube.com'
+    , port: 80
+  }
+});
+*/
